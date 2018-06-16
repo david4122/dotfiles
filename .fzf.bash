a45690936d01ca27a1bd18d5e547b5eb5f3b90fa
david4122 <d122b185.2@gmail.com>
2018-06-16 23:07:35 +0200

    fzf config path now uses `pwd`; fixed install.bash

--- .fzf.bash
+++ .fzf.bash
@@ -1,15 +1,15 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/dawid/fzf/bin* ]]; then
-  export PATH="$PATH:/home/dawid/fzf/bin"
+if [[ ! "$PATH" == */fzf/bin* ]]; then
+	export PATH="$PATH:$HOME/fzf/bin"
 fi
 export FZF_DEFAULT_OPTS='--preview "(head -$LINES {} || ls -la {} || echo {}) 2>/dev/null" --color fg:238,bg:233,hl:121,fg+:245,bg+:235,hl+:121,info:144,prompt:161,spinner:135,pointer:135,marker:118 --no-bold'
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/dawid/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/fzf/shell/completion.bash" 2> /dev/null
 complete -F _fzf_path_completion -o bashdefault la
 
 # Key bindings
 # ------------
-source "/home/dawid/fzf/shell/key-bindings.bash"
+source "$HOME/fzf/shell/key-bindings.bash"
--- install.bash
+++ install.bash
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "Creating backup folder"
+rmdir $HOME/dotfiles_backup 2>/dev/null
+mkdir $HOME/dotfiles_backup || exit 1
 
 echo "Initializing submodules..."
 git submodule update --init --recursive
@@ -8,7 +11,7 @@ cat .bash_aliases >> $HOME/.bash_aliases
 
 (cd stderred && make) || exit 1
 
-echo "Configuring .bashrc"
+echo "Configuring .bashrc..."
 cat <<EOF >> $HOME/.bashrc
 [[ \$TERM =~ '256color' ]] && [ -f ~/prompt.sh ] && source ~/prompt.sh
 
@@ -16,10 +19,11 @@ export LD_PRELOAD="$HOME/dotfiles/stderred/build/libstderred.so\${LS_PRELOAD:+:\
 export STDERRED_ESC_CODE=\$(printf "\\e[38;2;255;85;85m")
 EOF
 
-(cd fzf && ./install.bash) || exit 1
+[ -f $HOME/.fzf.bash ] && mv $HOME/.fzf.bash $HOME/dotfiles_backup
+(cd fzf && ./install) || exit 1
+ln -s $(pwd)/fzf $HOME
+rm $HOME/.fzf.bash
 
-rmdir $HOME/dotfiles_backup 2>/dev/null
-mkdir $HOME/dotfiles_backup || exit 1
 files=(.vimrc .fzf.bash .tmux.conf .vim .tmux)
 for file in ${files[@]}; do
 	if [ -f $HOME/$file ]; then
@@ -31,4 +35,5 @@ for file in ${files[@]}; do
 	ln -s $(pwd)/$file $HOME
 done
 
+echo "All done"
 rmdir $HOME/dotfiles_backup 2>/dev/null
