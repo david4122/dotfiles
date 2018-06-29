aac1761b5b1cc688cbaff21817fea33a37617478
david4122 <d122b185.2@gmail.com>
2018-06-29 22:21:37 +0200

    fuzzy tmux pane finder added

--- .fzf.bash
+++ .fzf.bash
@@ -4,7 +4,7 @@ if [[ ! "$PATH" == *$HOME/.vim/bundle/fzf/bin* ]]; then
   export PATH="$PATH:$HOME/.vim/bundle/fzf/bin"
 fi
 export FZF_DEFAULT_OPTS='--preview "(head -$LINES {} || ls -la {} || echo {}) 2>/dev/null" --color fg:238,bg:233,hl:121,fg+:245,bg+:235,hl+:121,info:144,prompt:161,spinner:135,pointer:135,marker:118 --no-bold'
-export FZF_ALT_C_COMMAND='find . -type d 2>/dev/null'
+export FZF_ALT_C_COMMAND="find . -type d -not -path '*/.git/*' -not -name '.git'"
 
 # Auto-completion
 # ---------------
@@ -14,3 +14,17 @@ complete -F _fzf_path_completion -o bashdefault la
 # Key bindings
 # ------------
 source "$HOME/.vim/bundle/fzf/shell/key-bindings.bash"
+
+
+fzf_tmux_pane_switcher() {
+	local format current
+	format="#{session_id}:#{window_id}.#{pane_id}|#{session_name}: #{window_name} [ #{pane_current_command} ] - #{pane_title}"
+	current=$(tmux display-message -p "$format")
+	tmux lsp -aF "$format" \
+		| grep -v "^${current%\|*}" \
+		| fzf +m --prompt 'jump to pane> ' --exit-0 --select-1 \
+			--delimiter='\|' --with-nth=2 --preview='tmux capture-pane -pet {1} | tail -n $LINES' \
+			--preview-window=up \
+		| cut -d'|' -f1 \
+		| xargs tmux switch-client -t 2>/dev/null
+}
