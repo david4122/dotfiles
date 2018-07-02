a2ca7533ac9107f546da48362c0d55eb04222ce4
david4122 <d122b185.2@gmail.com>
2018-07-02 23:38:26 +0200

    small changes in fzf pane switcher

--- .fzf.bash
+++ .fzf.bash
@@ -17,20 +17,20 @@ source "$HOME/.vim/bundle/fzf/shell/key-bindings.bash"
 
 # Functions
 fzf_tmux_pane_switcher() {
-	local format current sep=','
+	local format current sep=$'\t'
 	local reset="\033[0m" active="\033[38;5;184m" pane="\033[37m" window="\033[38;5;121m"
 
-	format="#{session_id}:#{window_id}.#{pane_id}$sep[#{pane_active}#{window_active}] #{session_name}: #{window_name} [ #{pane_current_command} ] - #{pane_title}" 
+	format="#{session_id}:#{window_id}.#{pane_id}$sep[#{window_active}#{pane_active}]$sep#{session_name}: #{window_name} [ #{pane_current_command} ] - #{pane_title}" 
 	current=$(tmux display-message -p "$format")
 
 	tmux lsp -aF "$format" \
-		| grep -v "^${current%,*}" \
+		| grep -v "^${current%%$sep*}" \
 		| sort -rt"$sep" -k2 \
 		| sed "s/\[11]/`printf "$active*$reset  "`/; s/\[10]/`printf "$window+$reset  "`/; s/\[01]/`printf "$pane-$reset  "`/; s/\[00]/     /" \
 		| fzf +m --prompt 'jump to pane> ' --exit-0 --select-1 --ansi \
-			--delimiter="$sep" --with-nth=2 \
+			--cycle --delimiter="$sep" --with-nth=2,3 \
 			--preview='tmux capture-pane -pet {1} | tail -n $LINES' \
-			--preview-window=up \
+			--preview-window=up:60% \
 		| cut -d"$sep" -f1 \
 		| xargs tmux switch-client -t 2>/dev/null
 }
